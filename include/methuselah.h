3e033b8bfca8223b61ce7b8db347191600fdb6a9
daniel-dominic <d.d.wymark@gmail.com>
2020-09-22 08:29:29 -0700

    Work on code structure

--- include/methuselah.h
+++ include/methuselah.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <cmath>
 #include <memory>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 namespace methuselah {
 
+// Exceptions
+// ==========------------------------------------------------------------------
+class NotImplementedException : public std::runtime_error {
+ public:
+  NotImplementedException(const std::string& arg) : std::runtime_error(arg) {}
+  NotImplementedException() : NotImplementedException("") {}
+};
+
+// Cell
+// ====------------------------------------------------------------------------
 template <typename T>
 class Cell {
  public:
@@ -21,23 +34,43 @@ class Cell {
   bool const onBorder;
 };
 
+// Grid
+// ====------------------------------------------------------------------------
 std::vector<short> const VON_NEUMANN_2D_NEIGHBORHOOD{-3, -1, 1, 3};
 std::vector<short> const MOORE_2D_NEIGHBORHOOD{-4, -3, -2, -1, 1, 2, 3, 4};
 std::vector<short> const MOORE_1D_NEIGHBORHOOD{-1, 1};
 
-enum Geometry { BOUNDED_1D, TOROIDAL_1D, BOUNDED_2D, TOROIDAL_2D };
+enum BoundaryConditions { BOUNDED, TOROIDAL };
 
 template <typename T>
 class Grid {
  public:
-  Grid(std::vector<short> neighborOffsets, Geometry geometry) : neighborhood(neighborOffsets), geometry(geometry) {}
-  Grid() : neighborhood(MOORE_2D_NEIGHBORHOOD), geometry(TOROIDAL_2D) {}
+  Grid(size_t size, unsigned short int dimensions,
+       BoundaryConditions boundaryConditions, std::vector<short> neighborhood)
+      : size(size),
+        dimensions(dimensions),
+        boundaryConditions(boundaryConditions),
+        neighborhood(neighborhood),
+  {}
 
  private:
-  unsigned short const padding;
+  size_t const size;
+  size_t const padding;
+  unsigned short int const dimensions;
   std::vector<short> const neighborhood;
-  Geometry const geometry;
+  BoundaryConditions const boundaryConditions;
   std::vector<std::unique_ptr<Cell<T>>> cells;
+
+  // static size_t determinePadding(size_t size, short maxNeighborDistance,
+  //                                BoundaryConditions boundaryConditions) {
+  //   switch (boundaryConditions) {
+  //     case Geometry.BOUNDED:
+  //       return (int)(std::pow(2, dimensions)) * ;
+
+  //     default:
+  //       break;
+  //   }
+  // }
 };
 
 }  // namespace methuselah
