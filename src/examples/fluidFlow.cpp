2e868658339f4b7004e35cc2a696b898392e62d2
daniel-dominic <d.d.wymark@gmail.com>
2020-09-30 23:28:36 -0700

    Attempted 2D fluid flow again but still no luck

--- src/examples/fluidFlow.cpp
+++ src/examples/fluidFlow.cpp
@@ -9,7 +9,11 @@
 #include "gridRenderer.h"
 #include "methuselah.h"
 
-using namespace methuselah;
+using methuselah::EventHandler;
+using methuselah::Grid;
+using methuselah::Neighborhood;
+using methuselah::Ortho2DColorRenderer;
+using methuselah::Wrapping;
 
 constexpr unsigned int CELL_SIZE = 12;
 
@@ -47,80 +51,58 @@ double cosineSimilarity(std::vector<double> vectorA,
 
 // Fluid Flow
 // ==========
-
-struct FluidCell {
-  FluidCell(double xVelocity, double yVelocity, uint8_t fluid)
-      : xVelocity(xVelocity), yVelocity(yVelocity), fluid(fluid) {
-    output.insert(output.begin(), 8, 0);
-  }
-  double xVelocity;
-  double yVelocity;
-  uint8_t fluid;
-  std::vector<uint8_t> output;
-};
-
 constexpr uint8_t NUM_NEIGHBORS = 8;
 
-void update(FluidCell* cell, std::vector<FluidCell*> neighbors) {
-  std::fill(cell->output.begin(), cell->output.end(), 0);
+struct Cell {
+  Cell(uint8_t fluid, bool passable) : fluid(fluid), passable(passable) {
+    fluidOutput.resize(NUM_NEIGHBORS);
+  }
+  Cell(uint8_t fluid) : Cell(fluid, true) {}
+  Cell() : Cell(0, true) {}
 
-  auto fluid = cell->fluid;
-  auto xv = cell->xVelocity;
-  auto yv = cell->yVelocity;
+  Cell(const Cell& other) {
+    fluid = other.fluid;
+    passable = other.passable;
+    fluidOutput.resize(NUM_NEIGHBORS);
+    for (auto i = 0; i < NUM_NEIGHBORS; ++i) {
+      fluidOutput[i] = other.fluidOutput[i];
+    }
+  }
 
+  Cell& operator=(const Cell& other) {
+    fluid = other.fluid;
+    passable = other.passable;
+    fluidOutput.resize(NUM_NEIGHBORS);
+    for (auto i = 0; i < NUM_NEIGHBORS; ++i) {
+      fluidOutput[i] = other.fluidOutput[i];
+    }
+    return *this;
+  }
+
+  uint8_t fluid;
+  std::vector<uint8_t> fluidOutput;
+  bool passable;
+};
+
+void update(Cell* cell, std::vector<Cell*> neighbors) {
+  std::fill(cell->fluidOutput.begin(), cell->fluidOutput.end(), 0);
+  
   // Gain
-  for (auto i = 0; i < 8; ++i) {
-    auto movement = neighbors[i]->output[i];
+  for (auto i = 0; i < NUM_NEIGHBORS; ++i) {
+    auto movement = neighbors[i]->fluidOutput[i];
     cell->fluid = std::min(255, movement + cell->fluid);
   }
 
   // Loss
-  int maxLoss = (int)(std::abs(xv) + std::abs(yv));
-  int loss = std::min(255, maxLoss);
-
-  if (loss > 0) {
-    auto coord = std::vector<double>{0, 0};
-    auto velocity = std::vector<double>{xv, yv};
-    auto similarities = std::vector<double>();
-    for (auto y = -1; y <= 1; ++y) {
-      coord[1] = y;
-      for (auto x = -1; x <= 1; ++x) {
-        coord[0] = x;
-
-        if (x == 0 && y == 0) {
-          continue;
-        }
-
-        similarities.push_back(
-            std::max(cosineSimilarity(coord, velocity), 0.0));
-      }
-    }
-
-    auto similarityTotal = 0.0;
-    for (auto similarity : similarities) {
-      similarityTotal += similarity;
-    }
-
-    for (auto i = 0; i < similarities.size(); ++i) {
-      auto movement =
-          static_cast<uint8_t>((similarities[i] / similarityTotal) * loss);
-      cell->output[NUM_NEIGHBORS - i] = movement;
-      cell->fluid -= movement;
-    }
+  for (auto i = NUM_NEIGHBORS - 3; cell->fluid && i < NUM_NEIGHBORS; ++i) {
+    cell->fluidOutput[NUM_NEIGHBORS - i] = 5;
+    cell->fluid -= 5;
   }
-
-
-  // Velocity change
-  // cell->xVelocity = BETA1 * xv * (1.0 - xv) - GAMMA1 * xv * yv;
-  // cell->yVelocity = BETA2 * yv * (1.0 - yv) - GAMMA2 * xv * yv;
-
-  cell->xVelocity *= 0.98;
-  cell->yVelocity *= 1.0001;
 }
 
-std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> colorize(const FluidCell& cell) {
-  auto r = cell.fluid / 2;
-  auto g = cell.fluid / 2;
+std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> colorize(const Cell& cell) {
+  auto r = cell.fluid;
+  auto g = cell.fluid;
   auto b = cell.fluid;
   return {r, g, b, 255};
 }
@@ -132,16 +114,14 @@ double randUnitInterval() { return (double)(rand()) / (double)(RAND_MAX); }
 
 bool randBool() { return rand() % 2 == 0; }
 
-void randomize(Grid<FluidCell>& grid) {
+void randomize(Grid<Cell>& grid) {
   srand(time(0));
   auto coord = std::vector<size_t>{0, 0};
   for (auto i = 0; i < GRID_HEIGHT; ++i) {
     coord[1] = i;
     for (auto j = 0; j < GRID_WIDTH; ++j) {
       coord[0] = j;
-      double xv = j * .0015 + i * 0.075;
-      double yv = j * 0.05 + i * 0.060;
-      grid.setValue(coord, FluidCell{xv, yv, (uint8_t)(rand() % 255)});
+      grid.setValue(coord, Cell{(uint8_t)(rand() % 255)});
     }
   }
 }
@@ -151,16 +131,15 @@ void randomize(Grid<FluidCell>& grid) {
 
 int main() {
   {
-    auto grid = std::shared_ptr<Grid<FluidCell>>(
-        new Grid<FluidCell>{{GRID_WIDTH, GRID_HEIGHT},
-                            Wrapping::TOROIDAL,
-                            Neighborhood::MOORE,
-                            update,
-                            FluidCell{0.0, 0.0, 0}});
+    auto grid =
+        std::shared_ptr<Grid<Cell>>(new Grid<Cell>{{GRID_WIDTH, GRID_HEIGHT},
+                                                   Wrapping::TOROIDAL,
+                                                   Neighborhood::MOORE,
+                                                   update});
     randomize(*grid);
 
-    Ortho2DColorRenderer<FluidCell> renderer{grid,      colorize,     CELL_SIZE,
-                                       CELL_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT};
+    Ortho2DColorRenderer<Cell> renderer{grid,      colorize,     CELL_SIZE,
+                                        CELL_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT};
     EventHandler eventHandler;
     eventHandler.registerKeyDownAction(SDLK_r, [&]() { randomize(*grid); });
 
