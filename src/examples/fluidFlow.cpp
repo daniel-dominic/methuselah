7de3adcd29a2669ff38b65db88333154b6755b03
daniel-dominic <d.d.wymark@gmail.com>
2020-10-01 22:30:25 -0700

    Another sandpile

--- src/examples/fluidFlow.cpp
+++ src/examples/fluidFlow.cpp
@@ -15,16 +15,18 @@ using methuselah::Neighborhood;
 using methuselah::Ortho2DColorRenderer;
 using methuselah::Wrapping;
 
-constexpr unsigned int CELL_SIZE = 10;
+constexpr unsigned int CELL_SIZE = 4;
 
-constexpr bool USE_DELAY = true;
-constexpr unsigned int DELAY = 50;
+constexpr bool USE_DELAY = false;
+constexpr unsigned int DELAY = 20;
 
-constexpr unsigned short int GRID_WIDTH = 40;
-constexpr unsigned short int GRID_HEIGHT = 50;
+constexpr unsigned short int GRID_WIDTH = 400;
+constexpr unsigned short int GRID_HEIGHT = 200;
 
 constexpr unsigned short int WINDOW_WIDTH = GRID_WIDTH * CELL_SIZE;
 constexpr unsigned short int WINDOW_HEIGHT = GRID_HEIGHT * CELL_SIZE;
+
+constexpr uint8_t WATER_MAX = 15;
 
 // Helper Functions
 // ================
@@ -49,23 +51,40 @@ double cosineSimilarity(std::vector<double> vectorA,
 constexpr uint8_t NUM_NEIGHBORS = 8;
 
 struct Cell {
-  bool water;
+  uint8_t water;
   bool passable;
 };
 
 void update(Cell* cell, std::vector<Cell*> neighbors) {
-  if (!cell->water && neighbors[1]->water) {
-    cell->water = true;
+  for (auto i = 0; i < 3 && cell->water < WATER_MAX; ++i) {
+    if (neighbors[i]->water > cell->water) {
+      cell->water += 1;
+      return;
+    }
   }
-  else if(cell->water && !neighbors[6]->water && neighbors[6]->passable) {
-    cell->water = false;
+
+  for (auto i = 5; i < 8; ++i) {
+    if (neighbors[i]->passable && neighbors[i]->water < cell->water &&
+        neighbors[i]->water < WATER_MAX) {
+      cell->water -= 1;
+      return;
+    }
   }
+
+  // if (neighbors[1]->water > cell->water) {
+  //   cell->water += 1;
+  // } else if (neighbors[6]->water < cell->water && neighbors[6]->passable) {
+  //   cell->water -= 1;
+  // }
 }
 
 std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> colorize(const Cell& cell) {
-  uint8_t r{0},g{0},b{0};
+  uint8_t r{0}, g{0}, b{0};
   if (cell.water) {
-    r= 255; g = 255; b = 255;
+    auto amt = 255 * (cell.water / (double)(WATER_MAX));
+    r = amt;
+    g = amt;
+    b = amt;
   }
   return {r, g, b, 255};
 }
@@ -75,14 +94,14 @@ std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> colorize(const Cell& cell) {
 
 double randUnitInterval() { return (double)(rand()) / (double)(RAND_MAX); }
 
-void randomize(Grid<Cell>& grid, uint8_t mod = 4) {
-  srand(time(0));
+void randomize(Grid<Cell>& grid, uint8_t mod = WATER_MAX) {
+  srand(time(0)*100);
   auto coord = std::vector<size_t>{0, 0};
   for (auto i = 0; i < GRID_HEIGHT; ++i) {
     coord[1] = i;
     for (auto j = 0; j < GRID_WIDTH; ++j) {
       coord[0] = j;
-      grid.setValue(coord, Cell{(bool)(rand() % mod == 0), true});
+      grid.setValue(coord, Cell{(uint8_t)(rand() % mod), true});
     }
   }
 }
@@ -97,7 +116,7 @@ int main() {
                                                    Wrapping::BOUNDED,
                                                    Neighborhood::MOORE,
                                                    update,
-                                                   Cell{false, false}});
+                                                   Cell{0, false}});
     randomize(*grid);
 
     Ortho2DColorRenderer<Cell> renderer{grid,      colorize,     CELL_SIZE,
@@ -105,14 +124,14 @@ int main() {
     EventHandler eventHandler;
     eventHandler.registerKeyDownAction(SDLK_r, [&]() { randomize(*grid); });
 
-    auto paused = true;
+    auto paused = false;
     eventHandler.registerKeyDownAction(SDLK_p, [&]() { paused ^= true; });
 
     auto oneStep = false;
     eventHandler.registerKeyDownAction(SDLK_SPACE, [&]() { oneStep = true; });
 
-    eventHandler.registerMouseClickAction([&](int32_t x, int32_t y) { oneStep = true; });
-
+    eventHandler.registerMouseClickAction(
+        [&](int32_t x, int32_t y) { oneStep = true; });
 
     auto running = true;
     while (running) {
